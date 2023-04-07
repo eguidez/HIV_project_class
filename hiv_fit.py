4290d9659677c72395ec7752e8c832b22728d2fd
Emilie Guidez <emilie.guidez@ucdenver.edu>
2023-04-07 10:52:19 -0600

    hiv_fit.py is to fit experimental data to our viral load equation

---
+++ hiv_fit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  7 10:46:36 2023
+
+@author: emilieguidez
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from scipy.optimize import curve_fit
+# We fit experimental data to our viral load equation
